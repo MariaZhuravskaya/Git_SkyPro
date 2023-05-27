0b715b9d22db98a05e7d3394c1a35f68138e3465
Maria <maria.mandrykina@mail.com>
2023-05-27 22:17:47 +0300

    add get_str_title

--- str_func.py
+++ str_func.py
@@ -1,8 +1,10 @@
 
 def get_str_upper(string):
     """Все буквы переводятся в заглавные"""
+
     return string.upper()
 
 def get_str_title(string):
     """Все первык буквы заглавные"""
+
     return string.title()
